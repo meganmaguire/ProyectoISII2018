76e381ca20283ef791c6bb9a2e6d5d5785096e67
Maru <Maru@Maru-PC>
2018-10-15 13:11:46 -0300

    clase gaseosa

--- src/Código/Gaseosa.java
+++ src/Código/Gaseosa.java
@@ -9,6 +9,68 @@ package Código;
  *
  * @author Maru
  */
-public class Gaseosa {
+public class Gaseosa extends Producto{
+    private String nombre;
+    private String sabor;
+    private float contenido;
+    private float precioCosto;
+    private Stock stock;
+
+    public Gaseosa(String nombre, String sabor, float contenido, float precioCosto, Stock stock, float precioVenta) {
+        super(precioVenta);
+        this.nombre = nombre;
+        this.sabor = sabor;
+        this.contenido = contenido;
+        this.precioCosto = precioCosto;
+        this.stock = stock;
+    }
+
+    public Gaseosa(){
+        super();
+        this.nombre="";
+        this.sabor="";
+        this.contenido=0;
+        this.precioCosto=0;
+        this.stock= new Stock();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public float getContenido() {
+        return contenido;
+    }
+
+    public float getPrecioCosto() {
+        return precioCosto;
+    }
+
+    public Stock getStock(){
+        return stock;
+    }
     
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public void setContenido(float contenido) {
+        this.contenido = contenido;
+    }
+
+    public void setPrecioCosto(float precioCosto) {
+        this.precioCosto = precioCosto;
+    }
+    
+    public void setStock(Stock stock){
+        this.stock=stock;
+    }
 }
