86cdce74109ce4109bba660b83254824c3186894
Maru <Maru@Maru-PC>
2018-10-15 13:12:20 -0300

    clase artesanal

--- src/Código/Artesanal.java
+++ src/Código/Artesanal.java
@@ -9,6 +9,57 @@ package Código;
  *
  * @author Maru
  */
-public class Artesanal {
+public class Artesanal extends Cerveza{
+    private String color;
+    private String lupulo;
+    private String malta;
+    private Barril barril;
+
+    public Artesanal(String color, String lupulo, String malta, Barril barril, String nombre, String marca, String tipo, float precioCosto, float graduacionAlc, float contenido, float precioVenta) {
+        super(nombre, marca, tipo, precioCosto, graduacionAlc, contenido, precioVenta);
+        this.color = color;
+        this.lupulo = lupulo;
+        this.malta = malta;
+        this.barril = barril;
+    }
     
+    public Artesanal(){
+        super();
+        this.color="";
+        this.lupulo="";
+        this.malta="";
+        this.barril= new Barril();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getLupulo() {
+        return lupulo;
+    }
+
+    public String getMalta() {
+        return malta;
+    }
+    
+    public Barril getBarril(){
+        return barril;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setLupulo(String lupulo) {
+        this.lupulo = lupulo;
+    }
+
+    public void setMalta(String malta) {
+        this.malta = malta;
+    }
+    
+    public void setBarril(Barril barril){
+        this.barril=barril;
+    }
 }
