3344bf5158427ac47c0c75196442ff6139a0bdde
Maru <Maru@Maru-PC>
2018-10-15 13:10:53 -0300

    clase producto

--- src/Código/Producto.java
+++ src/Código/Producto.java
@@ -9,6 +9,22 @@ package Código;
  *
  * @author Maru
  */
-public class Producto {
+public abstract class Producto {
+    private float precioVenta;
+
+    public Producto(float precioVenta) {
+        this.precioVenta = precioVenta;
+    }
     
+    public Producto(){
+        this.precioVenta=0;
+    }
+
+    public float getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(float precioVenta) {
+        this.precioVenta = precioVenta;
+    }
 }
