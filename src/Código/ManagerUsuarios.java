b11778a7e36fdb8d0e88872e05c976dcf3446f77
Maru <Maru@Maru-PC>
2018-10-18 16:29:23 -0300

    clase manager usuarios

--- src/Código/ManagerUsuarios.java
+++ src/Código/ManagerUsuarios.java
@@ -5,10 +5,65 @@
  */
 package Código;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author Maru
  */
 public class ManagerUsuarios {
+    List <Usuario> listaUsuarios;
     
+    //Constructor
+    public ManagerUsuarios(){
+        listaUsuarios=new ArrayList();
+    }
+    
+    //Getters
+    public List<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+    
+    //Setters
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+    
+    //Metodos para el manejo del manager usuarios
+    public void cargarUsuarios(){
+        //cargar usuarios de la bd
+    }
+    
+    //si el usuario existe en la bd devuelve verdadero, sino falso
+    public boolean validarUsuario(Usuario us1){
+        Iterator i= listaUsuarios.iterator();
+        while(i.hasNext()){
+            Usuario usuario = (Usuario) i.next();
+            if(usuario.equals(us1)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void registrarUsuario(Usuario us1){
+        listaUsuarios.add(us1);
+    }
+    
+    public void modificarUsuario(Usuario us1, Usuario us2){
+        listaUsuarios.remove(us1);  //elimina el usuario a modificar
+        listaUsuarios.add(us2);     //agrega el usuario modificado
+    }
+    
+    public void eliminarUsuario(Usuario us1){
+        Iterator i= listaUsuarios.iterator();
+        while(i.hasNext()){
+            Usuario usuario = (Usuario) i.next();
+            if(usuario.equals(us1)){
+                listaUsuarios.remove(usuario);
+            }
+        }
+    }
 }
