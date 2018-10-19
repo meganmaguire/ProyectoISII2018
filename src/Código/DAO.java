1c647f94d5019dc898dddaa466ce4e37761cc7bc
Maru <Maru@Maru-PC>
2018-10-19 10:52:21 -0300

    Merge origin/master
    Conflicts:
    	src/Interfaz/Login.form
    	src/Interfaz/Login.java

--- src/Código/DAO.java
+++ src/Código/DAO.java
@@ -10,5 +10,45 @@ package Código;
  * @author Maru
  */
 public interface DAO {
+    //reades
+    //public void readeUsuarios();
+    public void readeVentas();
+    public void readeCompras();
+    public void readeIndustriales();
+    public void readeArtesanal();
+    public void readeTragos();
+    public void readeNarguiles();
+    public void readeGaseosas();
+    public void readeVinos();
+    public void readePizzas();
+    public void readePicadas();
+    
+    //creates
+    //public void createUsuario(Usuario usuario);
+    public void createVenta(Venta venta);
+    //public void createCompra(Compra compra);
+    //public void createIndustriales(Industrial industrial);
+    //public void createArtesanalas(Artesanal artesanal);
+    //public void createTragos(Trago trago);
+    //public void createNarguile(Narguile narguile);
+    //public void createGaseosa(Gaseosa gaseosa);
+    //public void createVino(Vino vino);
+    //public void createPizza(Pizza pizza);
+    //public void createPicada(Picada picada);
+    
+    
+    
+    //deletes
+    //public void deleteUsuario(Usuario usuario);
+    //public void deleteVenta(Venta venta);
+    //public void deleteCompra(Compra compra);
+    //public void deleteIndustriales(Industrial industrial);
+    //public void deleteArtesanalas(Artesanal artesanal);
+    //public void deleteTragos(Trago trago);
+    //public void deleteNarguile(Narguile narguile);
+    //public void deleteGaseosa(Gaseosa gaseosa);
+    //public void deleteVino(Vino vino);
+    //public void deletePizza(Pizza pizza);
+    //public void deletePicada(Picada picada);
     
 }
