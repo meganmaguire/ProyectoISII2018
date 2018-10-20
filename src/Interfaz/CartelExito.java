0968912f7505e1b4014b284812ccd636c6a78c3c
Megan <meganmaguire000@gmail.com>
2018-10-20 19:32:42 -0300

    actualizado

--- src/Interfaz/CartelExito.java
+++ src/Interfaz/CartelExito.java
@@ -14,9 +14,10 @@ public class CartelExito extends javax.swing.JDialog {
     /**
      * Creates new form CartelExito
      */
-    public CartelExito(java.awt.Frame parent, boolean modal) {
+    public CartelExito(java.awt.Frame parent, boolean modal,String mensaje) {
         super(parent, modal);
         initComponents();
+        this.labelMensaje.setText(mensaje);
     }
 
     /**
@@ -33,7 +34,7 @@ public class CartelExito extends javax.swing.JDialog {
         cerrar = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        labelMensaje = new javax.swing.JLabel();
         botonEliminar1 = new javax.swing.JPanel();
         labelEliminar1 = new javax.swing.JLabel();
 
@@ -83,10 +84,10 @@ public class CartelExito extends javax.swing.JDialog {
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Interfaz/icons/icons8_Checked_96px.png"))); // NOI18N
 
-        jLabel2.setFont(new java.awt.Font("Fira Sans Book", 0, 18)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(250, 250, 250));
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("<Mensaje de éxito>");
+        labelMensaje.setFont(new java.awt.Font("Fira Sans Book", 0, 18)); // NOI18N
+        labelMensaje.setForeground(new java.awt.Color(250, 250, 250));
+        labelMensaje.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelMensaje.setText("<Mensaje de éxito>");
 
         botonEliminar1.setBackground(new java.awt.Color(36, 46, 59));
         botonEliminar1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(69, 162, 158)));
@@ -121,7 +122,7 @@ public class CartelExito extends javax.swing.JDialog {
                 .addGap(50, 50, 50)
                 .addComponent(jLabel1)
                 .addGap(38, 38, 38)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 296, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(labelMensaje, javax.swing.GroupLayout.PREFERRED_SIZE, 296, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(70, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -135,7 +136,7 @@ public class CartelExito extends javax.swing.JDialog {
                 .addGap(28, 28, 28)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(labelMensaje, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(botonEliminar1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -194,7 +195,7 @@ public class CartelExito extends javax.swing.JDialog {
         }
         //</editor-fold>
 
-        /* Create and display the dialog */
+        /* Create and display the dialog 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 CartelExito dialog = new CartelExito(new javax.swing.JFrame(), true);
@@ -206,17 +207,17 @@ public class CartelExito extends javax.swing.JDialog {
                 });
                 dialog.setVisible(true);
             }
-        });
+        });*/
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel botonEliminar1;
     private javax.swing.JLabel cerrar;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel labelEliminar1;
+    private javax.swing.JLabel labelMensaje;
     // End of variables declaration//GEN-END:variables
 }
